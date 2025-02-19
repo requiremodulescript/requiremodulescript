8b1385268dc6bcc3900e8eede482a34bfaa875d7
bootstrapp <hypriii@proton.me>
2025-02-19 15:57:25 +0800

    [+] create file

---
+++ files/infyiff.lua
@@ -0,0 +1,4 @@
+--github forced me to create a file so i added this file instead! ignore this you can copy inf yield ig below
+--copyright dominant 2025
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
